737f9801f81e0be89a4d28fcb848ef2839dd1e7c
Jarina Fathima S <jarinafathima1855@gmail.com>
2020-01-28 13:20:07 +0530

    comitting a text file

---
+++ sol.java
@@ -0,0 +1,12 @@
+package day0;
+import java.util.*;
+public class sol {
+	public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        System.out.println("Hello, World.");
+        System.out.println(s);
+        sc.close();
+	}
+
+}
